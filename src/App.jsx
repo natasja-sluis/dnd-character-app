bfd02aa901c05526d4213401b840ce630b533f11
natasja-sluis <natasjag92@gmail.com>
2024-07-01 13:13:05 +0200

    added routing set-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Homepage from "./pages/Homepage/Homepage.jsx";
 
 function App() {
 
     return (
         <>
+
+            <Routes>
+                <Route path="/" component={Homepage} />
+            </Routes>
         </>
     )
 }
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,7 @@
+import "./homepage.css"
+
+function Homepage() {
+ return <h1>Homepage</h1>
+}
+
+export default Homepage;
