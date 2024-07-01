29fa70403c3707121b7ae6ab992ae9b47d0e9a41
natasja-sluis <natasjag92@gmail.com>
2024-07-01 14:03:58 +0200

    added styling to nav bar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,17 +3,40 @@ import {NavLink} from "react-router-dom";
 
 function Navigation() {
     return <nav>
-        <ul>
-            <li>
-                <NavLink to="/">Home</NavLink></li>
-            <li>
-                <NavLink to="/overview">All Classes</NavLink>
-            </li>
-            <li>
-                <NavLink to="/favourites">Favourites</NavLink>
-            </li>
-        </ul>
-        </nav>
+        <div className="navigation-container">
+            <h2 className="navigation-title">D&D</h2>
+            <ul className="navigation-inner-container">
+                <li className="navigation-item">
+                    <NavLink
+                        className= {({isActive}) => isActive ? "active-link" : "default-link"}
+                        to="/">Home
+                    </NavLink>
+                </li>
+                <li className="default-link">
+                    <img className="navigation-dot"
+                         src="/src/assets/icons/dot.svg"
+                         alt="dot"/>
+                </li>
+                <li className="navigation-item">
+                    <NavLink
+                        className= {({isActive}) => isActive ? "active-link" : "default-link"}
+                        to="/overview">All Classes
+                    </NavLink>
+                </li>
+                <li className="default-link">
+                    <img className="navigation-dot"
+                         src="/src/assets/icons/dot.svg"
+                         alt="dot"/>
+                </li>
+                <li className="navigation-item">
+                    <NavLink
+                        className= {({isActive}) => isActive ? "active-link" : "default-link"}
+                        to="/favourites">Favourites
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+    </nav>
 }
 
 export default Navigation;
