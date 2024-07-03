cfb306133eb1d5f4842b95c7558d87a45e79e04f
natasja-sluis <natasjag92@gmail.com>
2024-07-03 09:01:33 +0200

    cleaned up code

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,53 +8,55 @@ function Navigation() {
     const [menuOpen, setMenuOpen] = useState(false);
 
 
-    return <nav className="navigation-container">
-        <Link to="/">
-            <h2 className="navigation-title">D&D</h2>
-        </Link>
-        <div className="mobile-menu" onClick={() => setMenuOpen(!menuOpen)}>
-            {menuOpen ?
-                <XSquare
-                    size={32}
-                    color="hsl(34, 75%, 44%)"
-                /> :
-                <List
-                    size={32}
-                    color="hsl(34, 75%, 44%)"
-                />
-            }
-        </div>
-        <ul className={menuOpen && "open-menu"}>
-            <li className="navigation-item">
-                <NavLink
-                    className={({isActive}) => isActive ? "active-link" : "default-link"}
-                    to="/">Home
-                </NavLink>
-            </li>
-            <li className="default-link">
-                <Dot
-                    size={32}
-                />
-            </li>
-            <li className="navigation-item">
-                <NavLink
-                    className={({isActive}) => isActive ? "active-link" : "default-link"}
-                    to="/overview">All Classes
-                </NavLink>
-            </li>
-            <li className="default-link">
-                <Dot
-                    size={32}
-                />
-            </li>
-            <li className="navigation-item">
-                <NavLink
-                    className={({isActive}) => isActive ? "active-link" : "default-link"}
-                    to="/favourites">Favourites
-                </NavLink>
-            </li>
-        </ul>
-    </nav>
+    return <header>
+        <nav className="navigation-container">
+            <Link to="/">
+                <h2 className="navigation-title">D&D</h2>
+            </Link>
+            <div className="mobile-menu" onClick={() => setMenuOpen(!menuOpen)}>
+                {menuOpen ?
+                    <XSquare
+                        size={32}
+                        color="hsl(34, 75%, 44%)"
+                    /> :
+                    <List
+                        size={32}
+                        color="hsl(34, 75%, 44%)"
+                    />
+                }
+            </div>
+            <ul className={menuOpen && "open-menu"}>
+                <li className="navigation-item">
+                    <NavLink
+                        className={({isActive}) => isActive ? "active-link" : "default-link"}
+                        to="/">Home
+                    </NavLink>
+                </li>
+                <li className="default-link">
+                    <Dot
+                        size={32}
+                    />
+                </li>
+                <li className="navigation-item">
+                    <NavLink
+                        className={({isActive}) => isActive ? "active-link" : "default-link"}
+                        to="/overview">All Classes
+                    </NavLink>
+                </li>
+                <li className="default-link">
+                    <Dot
+                        size={32}
+                    />
+                </li>
+                <li className="navigation-item">
+                    <NavLink
+                        className={({isActive}) => isActive ? "active-link" : "default-link"}
+                        to="/favourites">Favourites
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
 }
 
 export default Navigation;
