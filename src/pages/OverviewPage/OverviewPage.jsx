60f3364f43f5b73bb179d5514f0eff8f39b414d3
natasja-sluis <natasjag92@gmail.com>
2024-07-08 09:20:16 +0200

    added unique images for each class

--- src/pages/OverviewPage/OverviewPage.jsx
+++ src/pages/OverviewPage/OverviewPage.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react'
 import axios from "axios";
 import {Heart} from "@phosphor-icons/react";
 import "./overviewPage.css"
+import getPicture from "../../utils/getPicture.js";
 
 function OverviewPage() {
     const [classes, setClasses] = useState({});
@@ -42,9 +43,8 @@ function OverviewPage() {
                     {loaded && !error && classes.results.map((result) => {
                         return <div className="class-card" key={result.name}>
                             <div className="image-container">
-                                <img
-                                    src="/src/assets/images/dnd-character-human-male-fighter-paladin-in-heavy-armor.jpg"
-                                    alt={result.name}/>
+                                <img src={getPicture(result.name)}
+                                     alt={result.name}/>
                                 <Heart
                                     className="favourite-icon"
                                 />
