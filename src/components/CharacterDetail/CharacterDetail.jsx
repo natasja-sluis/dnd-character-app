708dce2f0cf53cf560d7d2905dfdc67335068f81
natasja-sluis <natasjag92@gmail.com>
2024-07-14 09:58:30 +0200

    added responsiveness to details page

--- src/components/CharacterDetail/CharacterDetail.jsx
+++ src/components/CharacterDetail/CharacterDetail.jsx
@@ -29,94 +29,85 @@ function CharacterDetail({
                 <h1>{name}</h1>
                 <ul className="class-header-details-list">
                     <li>Hit dice: {hitdice}</li>
-                    <li>Spellcasting: {spellCasting}</li>
+                    {spellCasting && <li>SpellCasting: {spellCasting}</li>}
                     <li>Saves: {savingThrows}</li>
                 </ul>
             </div>
         </div>
 
-        <h2>Class Features</h2>
+        <div className="class-body-details">
 
-        <details>
-            <summary><h3>Hit Points</h3></summary>
-            <ul>
-                <li>
-                    Hit dice: {hitdice}
-                </li>
-                <li>
-                    HP at first level: {HPStart}
-                </li>
-                <li>
-                    Hit Points at Higher Levels: {HPLevelUp}
-                </li>
-            </ul>
-        </details>
+            <details>
+                <summary><h2>Class Features</h2></summary>
+                <h3>Hit Points</h3>
+                <ul>
+                    <li>
+                        Hit dice: {hitdice}
+                    </li>
+                    <li>
+                        HP at first level: {HPStart}
+                    </li>
+                    <li>
+                        Hit Points at Higher Levels: {HPLevelUp}
+                    </li>
+                </ul>
 
-        <details>
-            <summary>
+
                 <h3>Proficiencies</h3>
-            </summary>
-            <ul>
-                <li>
-                    Armor proficiency: {armor}
-                </li>
-                <li>
-                    Weapon proficiency: {weapons}
-                </li>
-                <li>
-                    Tool proficiency: {tools}
-                </li>
-                <li>
-                    Saving throws: {savingThrows}
-                </li>
-                <li>
-                    Skill proficiency: {skill}
-                </li>
-            </ul>
-        </details>
+                <ul>
+                    <li>
+                        Armor proficiency: {armor}
+                    </li>
+                    <li>
+                        Weapon proficiency: {weapons}
+                    </li>
+                    <li>
+                        Tool proficiency: {tools}
+                    </li>
+                    <li>
+                        Saving throws: {savingThrows}
+                    </li>
+                    <li>
+                        Skill proficiency: {skill}
+                    </li>
+                </ul>
 
-
-        <details>
-            <summary>
                 <h3>Equipment</h3>
-            </summary>
-            <ul>
-                <li>
-                    {equipment}
-                </li>
-            </ul>
-        </details>
+                <ul>
+                    <li>
+                        {equipment}
+                    </li>
+                </ul>
+            </details>
 
-        <h2>Class Abilities</h2>
+            <details>
+                <summary>
+                    <h2>Class Abilities</h2>
+                </summary>
+                <Markdown>{description}</Markdown>
+            </details>
 
-        <details>
-            <summary>
-                <h3>Spellcasting</h3>
-            </summary>
-            <Markdown>{description}</Markdown>
-        </details>
+            <details className="progression-table">
+                <summary>
+                    <h2>Class Progression</h2>
+                </summary>
+                <Markdown>{table}</Markdown>
+            </details>
 
-        <details>
-            <summary>
-                <h2>Class Progression</h2>
-            </summary>
-            <Markdown>{table}</Markdown>
-        </details>
-
-        <details>
-            <summary>
-                <h2>{subclassName}</h2>
-            </summary>
-            <ul>
-                {subclasses.map((subclass) => {
-                    return (<li key={subclass.slug}>
-                            {subclass.name}
-                        </li>
-                    )
-                })}
-            </ul>
-        </details>
-
+            <details>
+                <summary>
+                    <h2>{subclassName}</h2>
+                </summary>
+                <ul>
+                    {subclasses.map((subclass) => {
+                        return (<li key={subclass.slug}>
+                                {subclass.name}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </details>
+        </div>
     </div>
 }
 
