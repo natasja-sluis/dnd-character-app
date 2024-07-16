583666d953d33107af0d7d431c235597860c7db1
natasja-sluis <natasjag92@gmail.com>
2024-07-16 16:55:50 +0200

    added content to homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,30 @@
+import Button from "../../components/Button/Button.jsx";
+import {WaveSine} from "@phosphor-icons/react"
 import styles from "./Homepage.module.css"
 
+
+
+
 function Homepage() {
- return <h1>Homepage</h1>
+    return <div className={styles["homepage-container"]}>
+        <div className={styles["text-container"]}>
+            <h1 >Take the quiz</h1>
+            <WaveSine
+                size={23}
+            />
+        </div>
+        <div className={styles["button-container"]}>
+            <p className={styles["message"]}>Or...</p>
+            <Button
+                type="button"
+                text="All Classes"
+            />
+            <Button
+                type="button"
+                text="Favourites"
+            />
+        </div>
+    </div>
 }
 
 export default Homepage;
