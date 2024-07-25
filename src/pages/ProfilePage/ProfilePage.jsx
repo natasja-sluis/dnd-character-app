c94488602a2cf2646251d78cc8bf4789eb460a74
natasja-sluis <natasjag92@gmail.com>
2024-07-25 15:37:49 +0200

    fixed details on the favourites page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,4 +1,5 @@
 import {useContext} from "react";
+import {Link} from "react-router-dom";
 import {AuthContext} from "../../context/AuthContext.jsx";
 import styles from "./ProfilePage.module.css"
 import ClassTile from "../../components/ClassTile/ClassTile.jsx";
@@ -11,22 +12,25 @@ function ProfilePage() {
 
 
     return <section className={styles["profile-page"]}>
-        <p>Name: {user.username} </p>
-        <p>Email: {user.email}</p>
+        <h1>Welcome {user.username}!</h1>
+        <section className={styles["user-information"]}>
+            <p>Name: <em>{user.username} </em></p>
+            <p>Email: <em>{user.email}</em></p>
+        </section>
         <section className={styles["favourites-section"]}>
-            {favourites && <>
-                <h2>Favourites:</h2>
-                <div className={styles["inner-container"]}>
-                    {favourites.map((favourite) => {
-                        return <ClassTile
-                            key={favourite}
-                            characterName={favourite}
-                            slug={favourite.toLowerCase()}
-                        />
-                    })}
-                </div>
-            </>
-            }
+            <h2>Favourites:</h2>
+            {favourites ? <>
+            <div className={styles["inner-container"]}>
+                {favourites.map((favourite) => {
+                    return <ClassTile
+                        key={favourite}
+                        characterName={favourite}
+                        slug={favourite.toLowerCase()}
+                    />
+                })}
+            </div>
+        </> : <p>Browse <Link to="/overview">all classes</Link> to add your favourites here</p>
+        }
         </section>
     </section>
 }
